9c45a71b83c89daad59cb7777e7d7a68655064ab
epinaud <enzo.pinaud+42@protonmail.com>
2024-11-17 15:14:12 +0100

    Factorized and re-arranged stack functions;

---
+++ stack_push.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack_push.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: epinaud <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/15 16:46:48 by epinaud           #+#    #+#             */
+/*   Updated: 2024/11/16 20:54:35 by epinaud          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+// pa (push a) : take the first element at the top of b and 
+// put it at the top of a. Do nothing if b is empty.
+void	ps_pa(t_stack **a, t_stack **b, int output)
+{
+	t_stack	*tmp;
+
+	if (!*b)
+		return ;
+	tmp = *a;
+	*a = *b;
+	*b = (*b)->next;
+	(*a)->next = tmp;
+	if (output == 0)
+		write(1, "pa\n", 3);
+}
+
+// pb (push b) : take the first element at the top of a and put it 
+// at the top of b. Do nothing if a is empty.
+void	ps_pb(t_stack **stack_a, t_stack **stack_b, int output)
+{
+	t_stack	*tmp;
+
+	if (!*stack_a)
+		return ;
+	tmp = *stack_b;
+	*stack_b = *stack_a;
+	*stack_a = (*stack_a)->next;
+	(*stack_b)->next = tmp;
+	if (output == 0)
+		write(1, "pb\n", 3);
+}
---
+++ stack_swap.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack_swap.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: epinaud <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/14 22:16:43 by epinaud           #+#    #+#             */
+/*   Updated: 2024/11/16 20:54:35 by epinaud          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+// sa (swap a) : swap the first 2 elements at the top of stack a. 
+// Do nothing if there is only one or no elements).
+void	ps_sa(t_stack **a, int output)
+{
+	t_stack	*tmp;
+
+	if (!*a || !((*a)->next))
+		return ;
+	tmp = *a;
+	*a = (*a)->next;
+	tmp->next = (*a)->next;
+	(*a)->next = tmp;
+	if (output == 0)
+		write(1, "sa\n", 3);
+}
+
+// sb (swap b) : swap the first 2 elements at the top of stack b. 
+// Do nothing if there is only one or no elements).
+void	ps_sb(t_stack **b, int output)
+{
+	t_stack	*tmp;
+
+	if (!*b || !((*b)->next))
+		return ;
+	tmp = *b;
+	*b = (*b)->next;
+	tmp->next = (*b)->next;
+	(*b)->next = tmp;
+	if (output == 0)
+		write(1, "sb\n", 3);
+}
+
+// ss : sa and sb at the same time.
+void	ps_ss(t_stack **a, t_stack **b, int output)
+{
+	t_stack	*tmp;
+
+	if (!*a || !((*a)->next) || !*b || !((*b)->next))
+		return ;
+	tmp = *a;
+	*a = (*a)->next;
+	tmp->next = (*a)->next;
+	(*a)->next = tmp;
+	tmp = *b;
+	*b = (*b)->next;
+	tmp->next = (*b)->next;
+	(*b)->next = tmp;
+	if (output == 0)
+		write(1, "ss\n", 3);
+}
+
