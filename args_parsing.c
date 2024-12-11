7331e7b73f226a5a4d9d45af0eb272df9d07ee88
epinaud <enzo.pinaud+42@protonmail.com>
2024-12-11 21:54:39 +0100

    Added proper memory cleaning for usecases of ft_split; Added check so that push_swap only splits its arg when said arg has at least one space in it; Factorized t_stack; Patched split bug;

--- args_parsing.c
+++ args_parsing.c
@@ -6,7 +6,7 @@
 /*   By: epinaud <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/17 15:19:11 by epinaud           #+#    #+#             */
-/*   Updated: 2024/11/17 16:39:32 by epinaud          ###   ########.fr       */
+/*   Updated: 2024/12/11 22:03:04 by epinaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 void	lst_wipe(t_stack *lst)
 {
 	lst->nbr = 0;
-	lst->index = 0;
 }
 
 static int	check_dup(char *strnum, char **args)
@@ -29,18 +28,19 @@ static int	check_dup(char *strnum, char **args)
 	return (0);
 }
 
+//Take + signs into account ??
 static int	check_arg(char **args)
 {
-	size_t	nlen;
-	size_t	slen;
+	size_t		nlen;
+	size_t		slen;
 	long long	nbr;
-	
-	//Take + signs into account ??
+
 	slen = ft_strlen(*args);
 	nbr = ft_atoi(*args);
 	nlen = ft_nbrlen(nbr);
 	if (slen != nlen)
-		ft_printf("Slen is %u & nlen is %u \n For str %s & num %d\n", slen, nlen, *args, nbr);
+		ft_printf("Slen is %u & nlen is %u \n For str %s & num %d\n",
+			slen, nlen, *args, nbr);
 	if (nbr < INT_MIN || nbr > INT_MAX)
 		put_err("INT overflow within provided values\n");
 	if (check_dup(*args, args + 1))
@@ -53,23 +53,23 @@ int	parse_args(int argc, char **args, t_stack **stack_a)
 	char	**numstrlst;
 	t_stack	*node;
 	size_t	i;
-	
-	if (argc > 2)
+
+	if (argc > 2 || !ft_strchr(*args, ' '))
 		numstrlst = args;
 	else
-		numstrlst = ft_split(args[1], ' ');
+		numstrlst = ft_split(*args, ' ');
 	i = 0;
 	while (numstrlst[i])
 	{
 		if (check_arg(&numstrlst[i]))
-			return (1);
-		node = ft_lstnew(&(t_stack){.nbr = ft_atoi(numstrlst[i++])});
+			return (ft_clean_memtree(numstrlst), 1);
+		node = ft_lstnew(&(t_stack){ft_atoi(numstrlst[i++]), NULL});
 		if (!node)
 			ft_lstclear(stack_a, &lst_wipe);
 		else
 			ft_lstadd_back(stack_a, node);
 	}
 	if (argc == 2)
-		free(numstrlst);
+		free(ft_clean_memtree(numstrlst));
 	return (0);
 }
