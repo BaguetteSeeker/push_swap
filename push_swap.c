16fd097c0cf2071a3e785a84ef6e07726266211c
epinaud <enzo.pinaud+42@protonmail.com>
2025-01-25 23:08:10 +0100

    Added lst_put util to output a stack member;

--- push_swap.c
+++ push_swap.c
@@ -6,66 +6,17 @@
 /*   By: epinaud <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 02:07:57 by epinaud           #+#    #+#             */
-/*   Updated: 2025/01/10 20:43:52 by epinaud          ###   ########.fr       */
+/*   Updated: 2025/01/25 22:30:33 by epinaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	prep_stack(t_stack *stack, char ps)
+void	lst_put(t_nbrlst *lst)
 {
-	void	(*psptr)(LL_TYP **, int);
-	t_sort	moves;
-
-	moves = (t_sort){0};
-	if (ps == 'b')
-	{
-		eval_rots(get_pos(lst_max(stack->list),
-			stack->list), stack->size, &moves);
-		if (moves.side == up)
-			psptr = &ps_rb;
-		else
-			psptr = &ps_rrb;
-		while (moves.cost-- != 0)
-			psptr(&(stack->list), 0);
-	}
-	else
-	{
-		eval_rots(get_pos(lst_min(stack->list),
-			stack->list), stack->size, &stack->moves);
-		if (moves.side == up)
-			psptr = &ps_ra;
-		else
-			psptr = &ps_rra;
-		while (moves.cost-- != 0)
-			psptr(&(stack->list), 0);
-	}
-}
-
-int	get_dest_rev(int nbr, t_nbrlst *stack)
-{
-	int		dest;
-	long	spread;
-	t_nbrlst	*stk_head;
-
-	dest = 0;
-	spread = 0;
-	stk_head = stack;
-	while (stack)
-	{
-		if (nbr < stack->nbr)
-		{
-			if (stack->nbr - nbr < spread || spread == 0)
-			{
-				spread = stack->nbr - nbr;
-				dest = stack->nbr;
-			}
-		}
-		stack = stack->next;
-	}
-	if (spread == 0)
-		return (get_pos(lst_max(stk_head), stk_head) + 1);
-	return (get_pos(dest, stk_head));
+	ft_putstr_fd("Stack member has nbr : ", 1);
+	ft_putnbr_fd(lst->nbr, 1, 0);
+	ft_putendl_fd("", 1);
 }
 
 void	initial_push(t_nbrlst **stack_a, t_nbrlst **stack_b)
@@ -85,8 +36,8 @@ void	sort_list(t_nbrlst **stack_a, t_nbrlst **stack_b, t_stacks *stks)
 	else if (stks->a.size == 5)
 		return (sort_five(stack_a, stack_b, stks));
 	initial_push(stack_a, stack_b);
-	stks->a.size -= 2;
-	stks->b.size += 2;
+	stks->a.size -= INITIAL_STACKSIZ;
+	stks->b.size += INITIAL_STACKSIZ;
 	while (*stack_a)
 	{
 		fetch_cheapest(&(stks->a), &(stks->b), stks);
